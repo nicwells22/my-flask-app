902b8b8d677397410c2eb1b08f751521e5698d93
nicwells22 <nicwells22@gmail.com>
2019-07-20 21:34:46 -0500

    add templating

--- hello.py
+++ hello.py
@@ -73,6 +73,10 @@ def index(user):
 def display_marks(score):
     return render_template('marks.html', marks=score)
 
+@app.route('/result/')
+def result():
+    d = {'phy': 50, 'che': 60, 'maths': 70}
+    return render_template('result.html', result=d)
 
 if __name__ == '__main__':
     app.run(debug=True)
