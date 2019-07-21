153c059d7a20e4e3904fb33fd7c5b62fc1291e72
nicwells22 <nicwells22@gmail.com>
2019-07-20 21:01:10 -0500

    include several test url routes

--- hello.py
+++ hello.py
@@ -8,7 +8,35 @@ def hello_world():
 
 @app.route('/hello')
 def hello_world2():
-    return 'Hello World'
+    return 'Hello World2'
+
+def hello_world3():
+    return 'Hello World3'
+app.add_url_rule('/','hello3',hello_world3)
+
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'Hello {}!'.format(name)
+
+@app.route('/hello/<int:i>')
+def multi_hello(i):
+    return 'Hello World!<br>' * i
+
+@app.route('/hello/<float:f>')
+def float_hello(f):
+    return 'Hello World<br>Float Value: {}'.format(f)
+
+@app.route('/<str1>/<str2>')
+def test_multi_param(str1,str2):
+    return '{} {}'.format(str1,str2)
+
+
+@app.route('/<int:id>/hello/')
+def test_reverse(id):
+    return 'This page id is: {}<br>Hello Visitor!'.format(id)
+
+
+
 
 
 if __name__ == '__main__':
